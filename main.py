e3e9881ae3b0ee50969938b58e841a2da95ddc52
dandy <130924779+frogcronies@users.noreply.github.com>
2023-05-15 15:46:09 -0400

    Fixed comparison date, properly works now.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from datetime import datetime, timedelta, timezone
 rotation = ['Borderland Ruins', 'Seal Rock', 'Fields of Glory (Shatter)', 'Onsal Hakair']
 
 today = datetime.now(tz=timezone.utc)
-origin = datetime.fromtimestamp(1683813600, tz=timezone.utc)
+origin = datetime.fromtimestamp(1683986401, tz=timezone.utc)
 delta = (today - origin)
 offset = delta.days%4
 
-print("Today's FFXIV PVP map is "+rotation[offset]+".")
+print("Today's FFXIV PVP map is "+rotation[offset]+". \nThe PVP schedule is as follows: Borderland Ruins, Seal Rock, Fields of Glory (Shatter), Onsal Hakair.")
