60c6180ee55c4adfd19bfa6563003663b98a9309
dandy <130924779+frogcronies@users.noreply.github.com>
2023-06-02 15:24:57 -0400

    Borderland Ruins adjusted (deleted) :) ty yoship

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta, timezone
 
 # PVP Schedule
-rotation = ['Borderland Ruins', 'Seal Rock', 'Fields of Glory (Shatter)', 'Onsal Hakair']
+rotation = ['Seal Rock', 'Fields of Glory (Shatter)', 'Onsal Hakair']
 
 today = datetime.now(tz=timezone.utc)
 origin = datetime.fromtimestamp(1683986401, tz=timezone.utc)
 delta = (today - origin)
-offset = delta.days%4
+offset = delta.days%3
 
-print("Today's FFXIV PVP map is "+rotation[offset]+". \nThe PVP schedule is as follows: Borderland Ruins, Seal Rock, Fields of Glory (Shatter), Onsal Hakair.")
+print("Today's FFXIV PVP map is "+rotation[offset]+". \nThe PVP schedule is as follows: Seal Rock, Fields of Glory (Shatter), Onsal Hakair.")
